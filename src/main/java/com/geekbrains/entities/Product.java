6fe272701b0f857c1a7696c9a8489f7381bc8126
Ериков Константин Александрович <k.erikov@2gis.ru>
2019-02-14 18:19:35 +0700

    GeekBrains Spring (part 1). Homework after 2-nd lesson.
     Committer: Ериков Константин Александрович <k.erikov@2gis.ru>
    
     On branch lesson2
     Changes to be committed:
    	modified:   .gitignore
    	new file:   pom.xml
    	new file:   src/main/java/com/geekbrains/controllers/FormsController.java
    	new file:   src/main/java/com/geekbrains/controllers/MainController.java
    	new file:   src/main/java/com/geekbrains/controllers/ProductsController.java
    	new file:   src/main/java/com/geekbrains/controllers/StudentsController.java
    	new file:   src/main/java/com/geekbrains/entities/Product.java
    	new file:   src/main/java/com/geekbrains/entities/Student.java
    	new file:   src/main/java/com/geekbrains/launcher/Launcher.java
    	new file:   src/main/java/com/geekbrains/repositories/ProductsRepository.java
    	new file:   src/main/java/com/geekbrains/repositories/StudentsRepository.java
    	new file:   src/main/java/com/geekbrains/services/ProductsService.java
    	new file:   src/main/java/com/geekbrains/services/StudentsService.java
    	new file:   src/main/webapp/WEB-INF/beans.xml
    	new file:   src/main/webapp/WEB-INF/view/index.jsp
    	new file:   src/main/webapp/WEB-INF/view/info.jsp
    	new file:   src/main/webapp/WEB-INF/view/myform-result-adv.jsp
    	new file:   src/main/webapp/WEB-INF/view/myform-result.jsp
    	new file:   src/main/webapp/WEB-INF/view/myform.jsp
    	new file:   src/main/webapp/WEB-INF/view/product-form-result.jsp
    	new file:   src/main/webapp/WEB-INF/view/product-form.jsp
    	new file:   src/main/webapp/WEB-INF/view/student-form-result.jsp
    	new file:   src/main/webapp/WEB-INF/view/student-form.jsp
    	new file:   src/main/webapp/WEB-INF/web.xml
    	new file:   src/main/webapp/resources/css/main.css

---
+++ src/main/java/com/geekbrains/entities/Product.java
@@ -0,0 +1,34 @@
+package com.geekbrains.entities;
+
+public class Product {
+    private Long productID;
+    private String productTitle;
+    private Long productCost;
+
+    public Product() {
+    }
+
+    public Long getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Long productID) {
+        this.productID = productID;
+    }
+
+    public String getProductTitle() {
+        return productTitle;
+    }
+
+    public void setProductTitle(String productTitle) {
+        this.productTitle = productTitle;
+    }
+
+    public Long getProductCost() {
+        return productCost;
+    }
+
+    public void setProductCost(Long productCost) {
+        this.productCost = productCost;
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/product-form-result.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+
+<html>
+    <body>
+        <h1>Product Form Result</h1>
+        <br>
+        Product ID: ${product.productID}
+        <br>
+        Product Title: ${product.productTitle}
+        <br>
+        Product Cost: ${product.productCost}
+        <br>
+    </body>
+</html>
